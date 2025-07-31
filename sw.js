2847ae9a227490505fc23ee4e8ea7610e5622f46
Yuhaku21 <Yuhaku21.farhandian2124@gmail.com>
2025-07-31 14:42:09 +0800

    update

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js
+self.addEventListener("install", (e) => {
+  console.log("Service Worker installed");
+});
+
+self.addEventListener("fetch", function (event) {
+  // Simpel fetch logic
+});
